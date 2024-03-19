acc5261c8404d318be53d45283a088329494f1b7
kimcrowing <50307403+kimcrowing@users.noreply.github.com>
2024-03-19 19:34:03 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 source_url="https://github.com/kimcrowing/EmuELEC.git"
 echo " Welcome to build Emulelec-addon"
 echo "Downloading Emulelec"
-git clone -b dev $source_url
+git clone -b dev_4.3 $source_url
 
 #PROJECT=Amlogic-ce DEVICE=Amlogic-ng ARCH=aarch64 DISTRO=EmuELEC make image
 echo "Copy emuelec-addon.sh"
